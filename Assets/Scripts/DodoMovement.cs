140f141b115f912c72f6be82760e2414f6ff2d9a
Raiponz <spooki.leserpent@orange.fr>
2020-04-18 13:21:57 +0200

    Wander how wander  why

---
+++ Assets/Scripts/DodoMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodoMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody2D rb;
+
+    public Vector2 goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        Vector2 newPos = Vector2.MoveTowards(transform.position,goal, speed * Time.deltaTime);
+        rb.MovePosition(newPos);
+    }
+
+    
+}
--- Assets/Scripts/WanderingBehaviour.cs
+++ Assets/Scripts/WanderingBehaviour.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class WanderingBehaviour : StateMachineBehaviour
 {
+   
+   [SerializeField]
+   private float wanderingRadius;
+   [SerializeField]
+    private float minWatingTime;
+     [SerializeField]
+    private float maxWatingTime;
+    private DodoMovement movement;
+    private float nextMoveTime;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        nextMoveTime = Time.time;
+       movement = animator.GetComponent<DodoMovement>();
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       Wander();
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -33,4 +43,16 @@ public class WanderingBehaviour : StateMachineBehaviour
     //{
     //    // Implement code that sets up animation IK (inverse kinematics)
     //}
+        public void Wander(){
+        if(Time.time > nextMoveTime){
+            nextMoveTime+= Random.Range(minWatingTime,maxWatingTime);
+         movement.goal = GetNewGoal();  
+        }   
+    }
+    private Vector2 GetNewGoal(){
+        var angle = Random.value;
+        float x  = Mathf.Cos(angle) * wanderingRadius;
+        float y = Mathf.Sin(angle) * wanderingRadius;
+        return new Vector2(x,y);
+    }
 }
