d0e1d29f034ba20cd71040cf99a2574f53abb69d
Raiponz <spooki.leserpent@orange.fr>
2020-04-18 19:50:56 +0200

    draw rail

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform[] waypoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        for(int i=0;i<waypoints.Length;i++){
+        Gizmos.color = Color.yellow;
+        if(i == waypoints.Length -1){
+                Gizmos.DrawLine(waypoints[i].position, waypoints[0].position);
+        }else {
+            Gizmos.DrawLine(waypoints[i].position, waypoints[i+1].position);
+        }
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
