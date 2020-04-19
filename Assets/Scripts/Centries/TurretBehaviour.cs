fccccf4382982c6a4c8fead9f974eb8a268964eb
zessirb <zessirb@gmail.com>
2020-04-20 00:56:25 +0900

    New angle of attack that will leave no blind spot

--- Assets/Scripts/Centries/TurretBehaviour.cs
+++ Assets/Scripts/Centries/TurretBehaviour.cs
@@ -15,17 +15,17 @@ public class TurretBehaviour : MonoBehaviour
     private Rail rail;
     private Vector3 mousePos;
     // Start is called before the first frame update
-     public int nextPoint;
+    public int nextPoint;
     public int previousPoint;
 
     private Rigidbody2D rb;
     void Start()
     {
-            rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         mainCamera = Camera.main;
-          previousPoint = 0;
-       nextPoint = rail.waypoints.Length -1;
-       transform.position = (getPrevious() + getNext())/2;
+        previousPoint = 0;
+        nextPoint = 1;
+        transform.position = (getPrevious() + getNext()) / 2;
     }
 
     // Update is called once per frame
@@ -34,72 +34,70 @@ public class TurretBehaviour : MonoBehaviour
         mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         Debug.DrawLine(transform.position, mousePos, Color.blue);
         Vector3 delta = mousePos - transform.position;
-        float angle = (Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg);
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        if(Input.GetMouseButtonDown(0)){
+
+        Vector2 position2d = new Vector2(transform.position.x, transform.position.y);
+        float angleMin = Vector2.SignedAngle(getPrevious() - position2d, Vector2.right);
+        float angleMax = Vector2.SignedAngle(getNext() - position2d, Vector2.right);
+        float willenAngle = Mathf.Atan2(delta.y, delta.x) * -Mathf.Rad2Deg;
+
+        if (((willenAngle < angleMin - 90 && willenAngle > angleMin - 180) || (willenAngle > angleMax && willenAngle < angleMax + 90)))
+            willenAngle = angleMax;
+        else if (((willenAngle < angleMin && willenAngle > angleMin - 90) || (willenAngle > angleMax + 90 && willenAngle < angleMax + 180)))
+            willenAngle = angleMin;
+        transform.rotation = Quaternion.AngleAxis(-willenAngle, Vector3.forward);
+        if (Input.GetMouseButtonDown(0))
+        {
             Fire();
         }
-         var newPos = Vector2.zero;
-        if(Input.GetKey(KeyCode.A)){
-            newPos = Vector2.MoveTowards(transform.position,getNext(),10 *Time.deltaTime);
-             rb.MovePosition(newPos);
-          
+        var newPos = Vector2.zero;
+        if (Input.GetAxis("TurretMovement") < 0)
+        {
+            newPos = Vector2.MoveTowards(transform.position, getPrevious(), 10 * Time.deltaTime);
+            rb.MovePosition(newPos);
+
+        } else if (Input.GetAxis("TurretMovement") > 0)
+        {
+            newPos = Vector2.MoveTowards(transform.position, getNext(), 10 * Time.deltaTime);
+            rb.MovePosition(newPos);
         }
-        if(Input.GetKey(KeyCode.E)){
-            newPos = Vector2.MoveTowards(transform.position,getPrevious(),10 * Time.deltaTime);
-           rb.MovePosition(newPos);
-         
-        }     
     }
 
     private void Fire()
     {
-       Instantiate(arrow,cannon.position,cannon.rotation);
+        Instantiate(arrow, cannon.position, cannon.rotation);
     }
-     void OnTriggerEnter2D(Collider2D collider)
+
+    void OnTriggerEnter2D(Collider2D collider)
     {
-      Debug.Log("collision");
-      int index = FindIndex(collider.name);
-      if(index == nextPoint){
-          increase();
-      } else
-      if(index == previousPoint){
-          decrease();
-      }
-    }
-    private int FindIndex(string name){
-for(int i =0;i<rail.waypoints.Length;i++){
-        if(rail.waypoints[i].name == name){
-            return i;
+        int index = collider.transform.GetSiblingIndex();
+        if (index == nextPoint)
+        {
+            GoToNextWaypoint();
+        } else if (index == previousPoint)
+        {
+            GoToPreviousWaypoint();
         }
-      }
-      return 0;
+        Debug.Log("Between " + previousPoint + " and " + nextPoint);
     }
-     private void increase(){
-    
+
+    private void GoToNextWaypoint()
+    {
         previousPoint = nextPoint;
-       
-        if(nextPoint==rail.waypoints.Length -1){
-            nextPoint =0;
-        }else {
-             nextPoint++;
-        }
+        nextPoint = (nextPoint == rail.waypoints.Length - 1) ? 0 : ++nextPoint;
     }
-    private void decrease(){
-    
-         nextPoint = previousPoint;
-               
-        if(previousPoint==0){
-            previousPoint =rail.waypoints.Length -1;
-        }else {
-        previousPoint--;
-        }
+    private void GoToPreviousWaypoint()
+    {
+        nextPoint = previousPoint;
+        previousPoint = (previousPoint == 0) ? rail.waypoints.Length - 1 : --previousPoint;
     }
-        private Vector2 getNext(){
-        return rail.waypoints[nextPoint].position;
-    }
-        private Vector2 getPrevious(){
+
+    private Vector2 getPrevious()
+    {
         return rail.waypoints[previousPoint].position;
+    }
+    private Vector2 getNext()
+    {
+        return rail.waypoints[nextPoint].position;
     }
 
 }
